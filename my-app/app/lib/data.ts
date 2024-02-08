8b8d4d2e76fcef0e42227d28f4489f1f5811acec
TLRKiliann <philogenie@protonmail.com>
2024-02-08 16:08:23 +0100

    data comments created

---
+++ my-app/app/lib/data.ts
@@ -0,0 +1,16 @@
+import type { CommentsProps } from "./definitions";
+
+export const comments: CommentsProps[] = [
+    {
+        id: 1,
+        name: "Douglas"
+    },
+    {
+        id: 2,
+        name: "Jacklyne"
+    },
+    {
+        id: 3,
+        name: "Javier"
+    }
+];
