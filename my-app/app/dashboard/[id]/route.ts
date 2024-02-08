a3ebe8b18e7baa761dfb30a058d7ab69e0e16dae
TLRKiliann <philogenie@protonmail.com>
2024-02-08 16:07:03 +0100

    dynamic route-handlers with get (byId) & patch method created and tested by thunder-client

---
+++ my-app/app/dashboard/[id]/route.ts
@@ -0,0 +1,19 @@
+import { comments } from '@/app/lib/data';
+
+export async function GET(request: Request, {params}: {params: {id: string}}) {
+    const comment = comments.find(
+        (comment) => comment.id === parseInt(params.id) 
+    );
+    return Response.json(comment);
+};
+
+export async function PATCH(request: Request, {params}: {params: {id: string}}) {
+    const body = await request.json();
+    const {name} = body;
+    const index = comments.findIndex(
+        (comment) => comment.id === parseInt(params.id)
+    )
+    comments[index].name = name;
+    return Response.json(comments[index])
+}
+
