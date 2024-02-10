cd245a5e505dc00f97435b1576d16500a9767f01
TLRKiliann <philogenie@protonmail.com>
2024-02-10 13:58:31 +0100

    CRUD complete

--- my-app/app/comments/page.tsx
+++ my-app/app/comments/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, {useState, useEffect } from 'react'
+import UpdateComp from '../components/UpdateComp';
 
 type CommentsProps = {
     id: number;
@@ -12,6 +13,8 @@ export default function Comments() {
     const [data, setData] = useState<CommentsProps[]>([])
 
     const [user, setUser] = useState<string>("");
+
+    const [newUser, setNewUser] = useState<string>("");
 
     useEffect(() => {
         const caller = async () => {
@@ -25,14 +28,14 @@ export default function Comments() {
     }, []);
 
     // create new user
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         console.log(event?.target.value)
         setUser(event.target.value);
     };
 
     // update (to be improve)
-    const handleUpdateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setUser(event.target.value);
+    const handleUpdateChange = (event: React.ChangeEvent<HTMLInputElement>, id: number): void => {
+        setNewUser(event.target.value)
     };
 
     const handleSubmit = async (event: React.FormEvent<HTMLFormElement>, user: string) => {
@@ -66,6 +69,19 @@ export default function Comments() {
         console.log(updateName);
     }
 
+    const handleDelete = async (id: number) => {
+        const deleteUser = await fetch(`/api/comments/${id}`, {
+            method: "DELETE",
+            body: JSON.stringify({
+                id: id,
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        console.log(deleteUser);
+    }
+
     return (
         <>
             {data.map((d: CommentsProps) => (
@@ -84,13 +100,13 @@ export default function Comments() {
             {data.map((u: CommentsProps) => (
                 <form key={u.id} onSubmit={(event) => handleUpdateSubmit(event, u.id)}>
                     <h2>Update user {u.name}</h2>
-                    <input 
-                        type="text" 
-                        value={user} 
-                        onChange={(event) => handleUpdateChange(event)}
-                        placeholder={u.name}
+                    <UpdateComp
+                        id={u.id}
+                        name={u.name}
+                        newUser={newUser}
+                        handleUpdateChange={(event, id) => handleUpdateChange(event, id)}
+                        handleDelete={(id) => handleDelete(id)}
                     />
-                    <button type="submit">Update</button>
                 </form>
             ))}
         </>
