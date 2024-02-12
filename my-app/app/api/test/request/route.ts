ae529d7f8a8ba7ff62b02026d935f23b5b75a8bf
TLRKiliann <philogenie@protonmail.com>
2024-02-12 22:08:20 +0100

    some changes

--- my-app/app/api/test/request/route.ts
+++ my-app/app/api/test/request/route.ts
@@ -5,7 +5,7 @@ import { NextRequest } from "next/server";
 // http://localhost:3000/api/test/request
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
-    console.log(searchParams, "searchParams")
+    //console.log(searchParams, "searchParams")
     const query = searchParams.get("query");
     const filterComment = query
      ? comments.filter((comment: CommentsProps) => comment?.text?.includes(query))
