855ef14e44330a0dbf8c5f441a6d79a5d6cdd7ee
TLRKiliann <philogenie@protonmail.com>
2024-02-12 22:07:58 +0100

    GET with params.id to be reachable by request.nextUrl.searchParams

--- my-app/app/api/test/request/[id]/route.ts
+++ my-app/app/api/test/request/[id]/route.ts
@@ -1,15 +1,10 @@
 import { comments } from "@/app/lib/data";
 import { CommentsProps } from "@/app/lib/definitions";
-import { NextRequest } from "next/server";
 
 // http://localhost:3000/api/test/request
-export async function GET(request: NextRequest) {
-    const searchParams = request.nextUrl.searchParams;
-    console.log(searchParams, "searchParams")
-    
-    const query = searchParams.get("query");
-    const filterComment = query
-     ? comments.filter((comment: CommentsProps) => comment?.text?.includes(query))
-     : comments;
-    return Response.json(filterComment);
-};
+export async function GET(request: Request, {params}: {params: {id: string}}) {
+    const findById = comments.find(
+        (comment: CommentsProps) => comment.id === parseInt(params.id)
+    );
+    return Response.json(findById);
+}
