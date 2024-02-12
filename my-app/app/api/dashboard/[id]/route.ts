36bd7473bc9f94e3855a7915cdbcfacbccd2ebbc
TLRKiliann <philogenie@protonmail.com>
2024-02-12 15:18:08 +0100

    some changes

--- my-app/app/api/dashboard/[id]/route.ts
+++ my-app/app/api/dashboard/[id]/route.ts
@@ -10,10 +10,19 @@ export async function GET(request: Request, {params}: {params: {id: string}}) {
 
 export async function PATCH(request: Request, {params}: {params: {id: string}}) {
     const body = await request.json();
-    const {name} = body;
+    const { name } = body;
     const index = comments.findIndex(
         (comment) => comment.id === parseInt(params.id)
     )
     comments[index].name = name;
     return Response.json(comments[index])
 };
+
+export async function DELETE(request: Request, {params}: {params: {id: string}}) {
+    const index = comments.findIndex(
+        (comment: CommentsProps) => comment.id === parseInt(params.id)
+    )
+    const deleteComment = comments[index];
+    comments.splice(index, 1);
+    return Response.json(deleteComment);
+}
