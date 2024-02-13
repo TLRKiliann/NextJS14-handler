5d7af7e543f2330f91033ddfa4c88b59b26af980
TLRKiliann <philogenie@protonmail.com>
2024-02-13 13:41:26 +0100

    some changes

--- my-app/app/api/test/request/[id]/route.ts
+++ my-app/app/api/test/request/[id]/route.ts
@@ -1,8 +1,12 @@
 import { comments } from "@/app/lib/data";
 import { CommentsProps } from "@/app/lib/definitions";
+import { redirect } from "next/navigation";
 
 // http://localhost:3000/api/test/request
 export async function GET(request: Request, {params}: {params: {id: string}}) {
+    if (parseInt(params.id) > comments.length) {
+        redirect("/comments")
+    }
     const findById = comments.find(
         (comment: CommentsProps) => comment.id === parseInt(params.id)
     );
